a305b6dfc9f01fac5a747716a8853220cf3b6687
Jason DeMorrow <jasondemorrow@gmail.com>
2026-01-04 09:47:18 -0800

    Fix migration code to use correct golang-migrate API

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/frkr-io/frkr-common
 go 1.18
 
 require (
-	github.com/golang-migrate/migrate/v4 v4.19.1
+	github.com/golang-migrate/migrate/v4 v4.17.0
 	golang.org/x/oauth2 v0.30.0
 )
 
 require (
 	github.com/cockroachdb/cockroach-go/v2 v2.1.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 )
--- internal/migrate/migrate.go
+++ internal/migrate/migrate.go
@@ -1,41 +1,19 @@
 package migrate
 
 import (
-	"database/sql"
 	"fmt"
 
 	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/cockroachdb"
-	"github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/golang-migrate/migrate/v4/database/cockroachdb"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 // RunMigrations runs all pending migrations
 func RunMigrations(dbURL string, migrationsPath string) error {
-	// Open database connection
-	db, err := sql.Open("postgres", dbURL)
-	if err != nil {
-		return fmt.Errorf("failed to open database: %w", err)
-	}
-	defer db.Close()
-
-	// Create CockroachDB driver
-	driver, err := cockroachdb.WithInstance(db, &cockroachdb.Config{})
-	if err != nil {
-		return fmt.Errorf("failed to create driver: %w", err)
-	}
-
-	// Create file source
-	source, err := (&file.File{}).Open(migrationsPath)
-	if err != nil {
-		return fmt.Errorf("failed to open migrations: %w", err)
-	}
-
 	// Create migrate instance
-	m, err := migrate.NewWithInstance(
-		"file",
-		source,
-		"cockroachdb",
-		driver,
+	m, err := migrate.New(
+		"file://"+migrationsPath,
+		dbURL,
 	)
 	if err != nil {
 		return fmt.Errorf("failed to create migrate instance: %w", err)
@@ -52,27 +30,9 @@ func RunMigrations(dbURL string, migrationsPath string) error {
 
 // GetVersion returns the current migration version
 func GetVersion(dbURL string, migrationsPath string) (uint, bool, error) {
-	db, err := sql.Open("postgres", dbURL)
-	if err != nil {
-		return 0, false, fmt.Errorf("failed to open database: %w", err)
-	}
-	defer db.Close()
-
-	driver, err := cockroachdb.WithInstance(db, &cockroachdb.Config{})
-	if err != nil {
-		return 0, false, fmt.Errorf("failed to create driver: %w", err)
-	}
-
-	source, err := (&file.File{}).Open(migrationsPath)
-	if err != nil {
-		return 0, false, fmt.Errorf("failed to open migrations: %w", err)
-	}
-
-	m, err := migrate.NewWithInstance(
-		"file",
-		source,
-		"cockroachdb",
-		driver,
+	m, err := migrate.New(
+		"file://"+migrationsPath,
+		dbURL,
 	)
 	if err != nil {
 		return 0, false, fmt.Errorf("failed to create migrate instance: %w", err)
